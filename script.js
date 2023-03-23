424366bdeecca5763e13e44ac2dee3f4dbbd3131
Mohamed Fazil M <fazilmm860@gmail.com>
2023-03-23 16:36:42 +0530

    logic started

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 'use strict';
-console.log(document.querySelector('.message')); 
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎆🎆 Correct Number'
+
+document.querySelector('.number').textContent = 12;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+
+
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value)
+    console.log(typeof guess);
+    if (!guess) {
+        document.querySelector('.message').textContent = '⛔ No Number!'
+    }
+
+})
